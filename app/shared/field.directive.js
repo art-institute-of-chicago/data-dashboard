17d05aaf47dce2a85286d3e1fda4fbbb50c9f415
Illya Moskvin <imoskvin@artic.edu>
2017-06-26 17:01:21 -0500

    Show arrays as unordered lists in field directive

--- app/shared/field.directive.js
+++ app/shared/field.directive.js
@@ -10,10 +10,22 @@
     function Directive( MapperService ) {
         return {
             restrict: 'E',
-            template: '<div class="row">'
-                    + '<div class="col-sm-3 text-ellipsis"><strong title="{{ label }}">{{ label }}</strong></div>'
-                    + '<div class="col-sm-9">{{ value }}</div>'
-                    + '</div>',
+            template: `
+                <div class="row">
+                    <div class="col-sm-3 text-ellipsis">
+                        <strong title="{{ label }}">{{ label }}</strong>
+                    </div>
+                    <div class="col-sm-9">
+
+                        <ul ng-if="isArray( value )">
+                            <li ng-repeat="item in value">{{ item }}</li>
+                        </ul>
+
+                        <span ng-if="!isArray( value )">{{ value }} </span>
+
+                    </div>
+                </div>
+            `,
             scope: {
                 key: '@',
                 value: '=',
@@ -21,6 +33,17 @@
             link: function( scope, element, attr ) {
 
                 scope.label = MapperService.getLabel( scope.key );
+
+                scope.isArray = function( value ) {
+
+                    // Wait until the value is ready...
+                    if( !value ) {
+                        return false;
+                    }
+
+                    return value.constructor === Array;
+
+                }
 
             }
         }
