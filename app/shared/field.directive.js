84ead93086b201a236408f3844821696bb019f45
Illya Moskvin <imoskvin@artic.edu>
2017-06-26 16:42:36 -0500

    Add MapperService for transformations

--- app/shared/field.directive.js
+++ app/shared/field.directive.js
@@ -5,18 +5,23 @@
         .module('app')
         .directive('field', Directive);
 
-    Directive.$inject = [];
+    Directive.$inject = ['MapperService'];
 
-    function Directive() {
+    function Directive( MapperService ) {
         return {
             restrict: 'E',
             template: '<div class="row">'
-                    + '<div class="col-sm-3 text-ellipsis"><strong title="{{ key }}">{{ key }}</strong></div>'
+                    + '<div class="col-sm-3 text-ellipsis"><strong title="{{ label }}">{{ label }}</strong></div>'
                     + '<div class="col-sm-9">{{ value }}</div>'
                     + '</div>',
             scope: {
                 key: '@',
                 value: '=',
+            },
+            link: function( scope, element, attr ) {
+
+                scope.label = MapperService.getLabel( scope.key );
+
             }
         }
     }
