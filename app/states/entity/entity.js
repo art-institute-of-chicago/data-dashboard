a5c4810e6775697c687fc26d596045e532f27586
Illya Moskvin <imoskvin@artic.edu>
2017-06-27 12:18:31 -0500

    Add form to find resources by id

--- app/states/entity/entity.js
+++ app/states/entity/entity.js
@@ -4,13 +4,46 @@
         .module('app')
         .controller('EntityController',  Controller);
 
-    Controller.$inject = [];
+    Controller.$inject = ['$state'];
 
-    function Controller() {
+    function Controller( $state ) {
 
         var vm = this;
 
+        vm.models = null;
+        vm.form = null;
+        vm.open = open;
+
+        activate();
+
         return vm;
+
+        function activate() {
+
+            vm.models = [
+                {
+                    name: 'Artwork',
+                    service: 'ArtworkService',
+                    states: {
+                        entity: 'entity.artwork',
+                    },
+                },
+            ];
+
+            vm.form = {
+                state: vm.models[0].states.entity,
+                id: null,
+            };
+
+        }
+
+        function open( state, id ) {
+
+            console.log( arguments );
+
+            $state.go( state, { id: id } );
+
+        }
 
     }
 
