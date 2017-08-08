c3291e7f5358c8498f2a3283c908cb4a233063f0
Illya Moskvin <imoskvin@artic.edu>
2017-08-08 14:38:50 -0500

    Remove Solr capabilities from SearchService

--- app/services/search.service.js
+++ app/services/search.service.js
@@ -10,56 +10,20 @@
     // TODO: Use the API service, somehow?
     function Service( $http, $q, esFactory ) {
 
-        // TODO: Don't hardcode the search URL
-        var solr_url = "http://localhost:8983/solr/data_aggregator/select";
-
         var elastic = esFactory({
             host: "http://localhost:9200/",
         });
 
         // define public interface
         return {
-            get: es, // switch this b/w `solr` and `es`
+            get: search,
         };
 
+
         // returns an angular promise
-        // performs a strict search for phrase
-        function solr( params ) {
+        function search( params ) {
 
-            // Standardize resolution b/w Solr and ES
-            var deferred = $q.defer();
-
-            $http.get( solr_url, {
-                params: {
-                    wt: 'json',
-                    rows: params.rows,
-                    start: params.start,
-                    q: params.query || "*.*",
-                }
-            }).then( function( response ) {
-
-                deferred.resolve({
-
-                    results: response.data.response.docs,
-                    total: response.data.response.numFound,
-                    start: response.data.response.start,
-
-                });
-
-            }, function( response ) {
-
-                deferred.reject( response );
-
-            });
-
-            return deferred.promise;
-
-        }
-
-
-        function es( params ) {
-
-            // Standardize resolution b/w Solr and ES
+            // Standardize resolution
             var deferred = $q.defer();
 
             elastic.search({
