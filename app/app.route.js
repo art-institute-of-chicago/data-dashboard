a8ff15fdbf77d2201cb391790adf3075e2ed3815
Illya Moskvin <imoskvin@artic.edu>
2017-11-27 01:06:04 -0600

    Nest search states under a new "browse" state
    This creates space to add a sidebar and alternative modes of exploring
    the collection, completely unrelated to the current Smart-Table-driven
    paradigm. I'm committing this, since it seems stable, but some naming
    changes are required.

--- app/app.route.js
+++ app/app.route.js
@@ -22,14 +22,14 @@
 
             // console.log( config );
 
-            if( config.name.match(/^root.entity\./) ) {
+            if( config.name.match(/^root.browse.entity\./) ) {
 
                 config.views = config.views || {};
 
                 // Relatively targets the unnamed view in this state's parent state.
                 var view = config.views[''] = config.views[''] || {};
 
-                view.templateUrl = view.templateUrl || 'states/entity/default/default.html';
+                view.templateUrl = view.templateUrl || 'states/browse/entity/default/default.html';
                 view.controller = view.controller || 'DefaultEntityController';
                 view.controllerAs = 'vm';
 
@@ -44,30 +44,38 @@
             .state('redirect', {
                 url: '/',
                 redirectTo: {
-                    state: 'root.entity.artworks',
+                    state: 'root.browse.entity.artworks',
                     params: { id: null }
                 },
             })
-            // Use as parent state to split screen b/w search + detail
             .state('root', {
                 abstract: true,
                 // Omit URL so that it's not prepended to everything
                 views: {
-                    'list@': {
-                        templateUrl: 'states/search/search.html',
+                    'main@': {
+                        templateUrl: 'states/browse/browse.html',
+                        controller: function() { },
+                    },
+                },
+            })
+            // Use as parent state to split screen b/w search + detail
+            .state('root.browse', {
+                url: '/browse',
+                views: {
+                    'list': {
+                        templateUrl: 'states/browse/search/search.html',
                         controller: 'SearchController',
                         controllerAs: 'vm',
                     },
-                },
-
+                }
             })
             // Use as parent state to add topbar
-            .state('root.entity', {
+            .state('root.browse.entity', {
                 abstract: true,
                 // Omit URL so that it's not prepended to everything
                 views: {
-                    'detail@': {
-                        templateUrl: 'states/entity/entity.html',
+                    'detail@root': {
+                        templateUrl: 'states/browse/entity/entity.html',
                         controller: 'EntityController',
                         controllerAs: 'vm',
                     },
@@ -76,13 +84,13 @@
                     cssClassnames: 'aic-state-entity'
                 }
             })
-            .state('root.entity.artworks', {
+            .state('root.browse.entity.artworks', {
                 url: '/artworks/:id',
                 params: {
                     model: 'ArtworkService'
                 }
             })
-            .state('root.entity.agents', {
+            .state('root.browse.entity.agents', {
                 url: '/agents/:id',
                 params: {
                     model: 'AgentService'
